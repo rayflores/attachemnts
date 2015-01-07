2385c4e84a5e528fb57c1efdbfcff2e77e5fdaa7
Ray Flores <rflores@socious.com>
2015-01-07 13:25:31 -0700

    display attachments

---
+++ display_attatchments.php
@@ -0,0 +1,18 @@
+<?php 
+
+	$attachments = new Attachments( 'my_attachments' ); 	
+	$images = array();
+	$images[] = wp_get_attachment_url(get_post_thumbnail_id($post->ID));
+?>
+
+<div id="post-gallery">
+	<div id="post-gallery-big">
+		<img src="<?php echo $images[0]; ?>"/>
+	</div>
+	<div id="post-thumbnails">
+		<?php if( $attachments->exist() ) : 
+				   while( $attachments->get() ) : ?>
+		<div class="post-thumb-wrap"><?php echo $attachments->image( 'thumbnail' ); ?></div>
+		<?php endwhile;  endif; ?>
+	</div>
+</div>
